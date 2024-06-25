d1557fe58fa3df1aedba73e20675c0ea8905719f
anand suthar <anand.suthar@fexle.com>
2024-06-25 10:48:29 +0530

    new section added

--- script.js
+++ script.js
@@ -24,3 +24,40 @@ function addAnimation() {
     });
   });
 }
+$(document).ready(function () {
+  $(".wander-destination-slider").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots: true,
+    focusOnSelect: true,
+    infinite: true,
+    nextArrow: ".wander-next",
+    prevArrow: ".wander-prev",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          variableWidth: true,
+        },
+      },
+    ],
+  });
+});
